eccf7baa23156052b52d74930299faa8642ac22b
Diamant Lelouette <git@diamant.dev>
2022-07-31 18:15:59 +0200

    📝 Add logs for prebuild.js

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -1,11 +1,24 @@
 import { readdir, writeFileSync } from 'fs';
 
-// Get number of images in the rdmImages folder.
+console.log('⚙️ Prebuild script started');
+let gtimer = Date.now();
+
+console.log('🖼️ Getting number of images in the logos folder.');
+let imageTimer = Date.now();
+// Get number of images in the logos folder.
 const images = readdir('./public/logos', (err, files) => {
   if (err) {
     throw new Error(err);
   } else {
+    console.log(
+      `🖼️ ${files.length} images found. (${Date.now() - imageTimer}ms)`
+    );
     const results = { nbrOfImages: files.length, files: files };
+    console.log(`📝 Writing results to file.`);
+    imageTimer = Date.now();
     writeFileSync('./src/logo.json', JSON.stringify(results));
+    console.log(`📝 Results written to file. (${Date.now() - imageTimer}ms)`);
   }
 });
+
+console.log(`✅ Prebuild script finished in ${Date.now() - gtimer}ms.`);
