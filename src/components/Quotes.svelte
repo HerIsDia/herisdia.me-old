0a2b9d5ea3703fc430090583c57ea910d2df5ae8
Diamant Lelouette <git@diamant.dev>
2022-08-01 06:32:12 +0200

    ➕ Some animations.

--- src/components/Quotes.svelte
+++ src/components/Quotes.svelte
@@ -2,16 +2,47 @@
   import * as quotes from '../quotes.json';
   export let lang: string = 'en';
 
-  const quotesInLanguage = quotes[lang];
-  const randomQuote =
-    quotesInLanguage[Math.floor(Math.random() * quotesInLanguage.length)];
+  const generateQuote = () => {
+    const quotesInLanguage = quotes[lang];
+    const randomQuote =
+      quotesInLanguage[Math.floor(Math.random() * quotesInLanguage.length)];
+    return randomQuote;
+  };
 
-  export let content: string = randomQuote;
+  export let content: string = generateQuote();
+  $: quotesToRender = content;
+
+  setInterval(() => {
+    quotesToRender = generateQuote();
+  }, 15000);
 </script>
 
 <div
   id="quotes"
   class="lg:bg-[#21052f] bg-[#371647] lg:mt-8 text-white pt-2 pb-2 lg:rounded-xl flex justify-center items-center"
 >
-  <p class="font-mundial text-[#ece0f3] italic font-thin">{@html content}</p>
+  <p class="quoteText font-mundial text-[#ece0f3] italic font-thin">
+    {@html quotesToRender}
+  </p>
 </div>
+
+<style>
+  .quoteText {
+    animation: quotes 15s infinite 0s;
+  }
+
+  @keyframes quotes {
+    0% {
+      opacity: 0;
+    }
+    10% {
+      opacity: 1;
+    }
+    90% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+</style>
