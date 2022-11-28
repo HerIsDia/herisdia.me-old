19ee09b8a704faf1a0506197a906eb2409286ca1
Diamant Lelouette <git@diamant.dev>
2022-11-28 23:12:09 +0100

    🛠️ Quick fix. (Thanks to @devwerewolf for the help.)

--- src/components/Box.svelte
+++ src/components/Box.svelte
@@ -1,6 +1,6 @@
 <div
   id="box"
-  class="text-white  overflow-y-auto overflow-x-hidden flex items-center h-screen pt-16 pb-16 pl-4 pr-4 "
+  class="text-white overflow-y-auto overflow-x-hidden grid items-center h-screen pt-16 pb-16 pl-4 pr-4 "
 >
   <div id="boxcontent" class="block-inline font-mundial w-full text-[#ece0f3]">
     <slot />
